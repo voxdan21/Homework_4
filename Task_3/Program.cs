b3c34bbd3c76db7b25789867ad9b9ea9ccc0297c
Voxdan21 <popov.daniil21@gmail.com>
2022-09-12 13:17:00 +0300

    Задание №3

---
+++ Task_3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Элементы массива вводятся пользователем.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+string MassNumber(int a, int b, int c, int d, int e, int f, int g, int h)
+{
+    int[] newMass = new int[8] { a, b, c, d, e, f, g, h };
+    string result = "";
+    for (int i = 0; i < 8; i++)
+    {
+        if (i == 7) result = result + newMass[i];
+        else result = result + newMass[i] + ",";
+    }
+
+    return result;
+}
+
+
+try
+{
+    Console.Write("Введите число 1: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 2: ");
+    int b = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 3: ");
+    int c = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 4: ");
+    int d = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 5: ");
+    int e = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 6: ");
+    int f = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 7: ");
+    int g = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 8: ");
+    int h = int.Parse(Console.ReadLine());
+
+    string result = MassNumber(a, b, c, d, e, f, g, h);
+    Console.WriteLine($"Массив из введенных элементов = [{result}]");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}

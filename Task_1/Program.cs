0a338cb8d322eca87c0b4103c21ef0ce56b92838
Voxdan21 <popov.daniil21@gmail.com>
2022-09-12 12:56:55 +0300

    Задание №1

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Решить задачу с использованием методов.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double NaturalDegree (int A, int B)
+{
+    double result = Math.Pow(A,B);
+    return result;
+}
+
+try
+{   
+    Console.Write("Введите число A: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int b = int.Parse(Console.ReadLine());
+    double result = NaturalDegree(a,b);
+    Console.WriteLine($"число {a} в натуральной степени {b} = {result}");
+}
+catch (Exception ex)
+{
+ Console.WriteLine($"Входное значение имеет неверный формат. Ошибка: {ex.Message}");
+}
